fcf212fb736d242b2a3f6f2d2fc261338119aa25
Nathan Owsiany <nowsiany@gmail.com>
2018-05-20 15:46:43 -0600

    Allow cors handler in grpc gateway

--- grpcgateway/main.go
+++ grpcgateway/main.go
@@ -1,41 +1,31 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"net/http"
 
 	"github.com/golang/glog"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-
-	gw "github.com/team-til/til/grpcgateway/_proto"
+	"github.com/team-til/til/grpcgateway/gateway"
 )
 
 var (
-	echoEndpoint = flag.String("echo_endpoint", "localhost:10000", "endpoint of YourService")
+	endpoint = flag.String("endpoint", "localhost:10000", "endpoint of the gRPC service")
+	network  = flag.String("network", "tcp", `one of "tcp" or "unix". Must be consistent to -endpoint`)
 )
-
-func run() error {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := gw.RegisterTilServiceHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
-	if err != nil {
-		return err
-	}
-
-	return http.ListenAndServe(":8080", mux)
-}
 
 func main() {
 	flag.Parse()
 	defer glog.Flush()
 
-	if err := run(); err != nil {
+	ctx := context.Background()
+	opts := gateway.Options{
+		Addr: ":8080",
+		GRPCServer: gateway.Endpoint{
+			Network: *network,
+			Addr:    *endpoint,
+		},
+	}
+	if err := gateway.Run(ctx, opts); err != nil {
 		glog.Fatal(err)
 	}
 }
