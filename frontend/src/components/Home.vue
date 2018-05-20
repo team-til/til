eee5b96158bbc92f22ce2ee16c8ab44e1c0140d0
Nathan Owsiany <nowsiany@gmail.com>
2018-05-20 12:33:25 -0600

    baic landing page

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero is-medium is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Team TIL - Today I Learned
+        </h1>
+        <h2 class="subtitle">
+          Share Knowledge Across Your Engineering Organization
+        </h2>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
