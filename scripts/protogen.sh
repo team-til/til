23b12f451a7030758c5772483a397fe6a099d525
Nathan Owsiany <nowsiany@gmail.com>
2018-05-19 23:23:16 -0600

    define basic til service proto

--- scripts/protogen.sh
+++ scripts/protogen.sh
@@ -7,9 +7,8 @@ mkdir -p ${PROJ_ROOT}/frontend/_proto
 
 protoc \
   --plugin=protoc-gen-ts=${PROJ_ROOT}/node_modules/.bin/protoc-gen-ts \
-  --plugin=protoc-gen-go=${GOBIN}/protoc-gen-go \
   -I ${PROJ_ROOT}/proto \
   --js_out=import_style=commonjs,binary:${PROJ_ROOT}/frontend/_proto \
   --go_out=plugins=grpc:${PROJ_ROOT}/server/_proto \
   --ts_out=service=true:${PROJ_ROOT}/frontend/_proto \
-${PROJ_ROOT}/proto/notes_service.proto
+${PROJ_ROOT}/proto/til_service.proto
